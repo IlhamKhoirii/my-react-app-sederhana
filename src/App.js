a822af20c5c032bcb20b358034c1c74986e2ebb9
triyono <yono.tri@gmail.com>
2024-11-18 19:46:59 +0700

    add use reducer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 
 import Header from './components/Header';
 import Counter from './components/Counter';
+import Counter2 from './components/Counter2';
 
 function App() {
   return (
@@ -14,12 +15,16 @@ function App() {
         <ul>
           <li><Link to="/">Beranda</Link></li>
           <li><Link to="/counter">Counter</Link></li>
+          <li><Link to="/counter2">Counter2</Link></li>
+
         </ul>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/counter" element={<Counter />} />
+        <Route path="/counter2" element={<Counter2 />} />
+
       </Routes>
     </div>
   );
---
+++ src/components/Counter2.js
@@ -0,0 +1,25 @@
+import React, { useReducer } from 'react';
+const initialState = { count: 0 };
+function reducer(angka, action) {
+    switch (action.type) {
+        case 'increment':
+            return { count: angka.count + 1 };
+        case 'decrement':
+            return { count: angka.count - 1 };
+        default:
+            throw new Error();
+    }
+}
+function Counter2() {
+    const [angka, fungsiMengubahNilai] = useReducer(reducer, initialState);
+    return (
+        <div>
+            Count: {angka.count}
+            <button onClick={() => fungsiMengubahNilai({ type: 'increment' })}>+</button>
+            <button onClick={() => fungsiMengubahNilai({ type: 'decrement' })}>-</button>
+        </div>
+    );
+}
+
+
+export default Counter2;
